af61fc40d4a519b27362d0fd723c33494655b2c6
Ramil <zarram89@gmail.com>
2025-08-22 15:35:19 +0500

    4.1.3 Добавит наследование компонентам

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,10 +1,10 @@
+import {render} from '../framework/render.js';
 import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
 import TaskListView from '../view/task-list-view.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
-import {render} from '../render.js';
 
 export default class BoardPresenter {
   boardComponent = new BoardView();
