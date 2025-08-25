68421e499feaa056263d174be2089bd0ae5b6fee
Ramil <zarram89@gmail.com>
2025-08-25 10:51:04 +0500

    4.1.7 Реализует смену карточки на форму

--- src/view/task-view.js
+++ src/view/task-view.js
@@ -65,13 +65,25 @@ function createTaskTemplate(task) {
 
 export default class TaskView extends AbstractView {
   #task = null;
+  #handleEditClick = null;
 
-  constructor({task}) {
+  constructor({task, onEditClick}) {
     super();
     this.#task = task;
+    this.#handleEditClick = onEditClick;
+
+    this.#handleEditClick = onEditClick;
+
+    this.element.querySelector('.card__btn--edit')
+      .addEventListener('click', this.#editClickHandler);
   }
 
   get template() {
     return createTaskTemplate(this.#task);
   }
+
+  #editClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleEditClick();
+  };
 }
